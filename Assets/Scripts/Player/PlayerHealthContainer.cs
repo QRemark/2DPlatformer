d12538b1e8f460cfdb9ae8adf24098f206f233da
QRemark <S099641108@yandex.ru>
2025-02-04 18:16:10 +0300

    dz ver 1

--- Assets/Scripts/Player/PlayerHealthContainer.cs
+++ Assets/Scripts/Player/PlayerHealthContainer.cs
@@ -1,24 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerHealthContainer : MonoBehaviour
 {
-    private float _baseHealth = 100;
+    private float _baseHealth = 145;
     private float _currentHealth;
+
+    public UnityEvent<float, float> OnHealthChanged = new UnityEvent<float, float>();
+
+    public float MaxHealth => _baseHealth;
+    public float CurrentHealth => _currentHealth;
 
     private void Start()
     {
         _currentHealth = _baseHealth;
+        OnHealthChanged.Invoke(_currentHealth, _baseHealth);
     }
 
     public void IncreasePlayerHealth(float healthRange)
     {
-        _currentHealth += healthRange;
-        Debug.Log(_currentHealth);
+        _currentHealth = Mathf.Clamp(_currentHealth + healthRange, 0, _baseHealth);
+        OnHealthChanged.Invoke(_currentHealth, _baseHealth);
     }
 
     public void ReducePlayerHealth(float attackRange)
     {
-        _currentHealth -= attackRange;
-        Debug.Log(_currentHealth);
+        _currentHealth = Mathf.Clamp(_currentHealth - attackRange, 0, _baseHealth);
+        OnHealthChanged.Invoke(_currentHealth, _baseHealth);
     }
 }
